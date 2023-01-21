5d3dcf7cfe52d4c2cfa0de3baffd733260dd0804
mohamed <mohamedox9080@gmail.com>
2023-01-21 08:56:57 +0000

    adding some mem funs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "vector.hpp"
 int main()
 {
-	vector<int> vec;
+	vector<int> vec(12,4);
 
 	vec.begin();
 }
--- vector.hpp
+++ vector.hpp
@@ -89,6 +89,9 @@ template < class T, class Alloc = std::allocator<T> > class vector{
 		};
 
 		typedef const iterator const_iterator;
+		typedef T& reference;
+		typedef size_t size_type;
+		typedef const T& const_reference;
 		typedef const revers_iterator const_revers_iterator;
 		iterator begin() { return iterator(&v_data[0]); }
 		iterator end()   { return iterator(&v_data[v_size - 1]); } 
@@ -98,35 +101,38 @@ template < class T, class Alloc = std::allocator<T> > class vector{
 		revers_iterator end(T)   { return revers_iterator(&v_data[0]); } 
 		const_revers_iterator begin(T)const { return revers_iterator(&v_data[v_size - 1]); }
 		const_revers_iterator end(T)  const { return revers_iterator(&v_data[0]); } 
-	// 	typedef typename iterator;
-	// 	typedef typename const_iterator;
+
 	// // constractors -----------------------------------------------------------------------------------------------------------
-	// 	vector(){
-	// 		v_data = alloc.allocate(1);
-	// 		v_size = 1;
-	// 	}
-	// 	vector(size_t n, T t){
-	// 		alloc.deallocate(v_data, v_size);
-	// 		v_data = alloc.allocate(n);
-	// 		for(int i = 0; i > n ; i++){
-	// 			v_data[i] = t;
-	// 		}
-	// 	}
-	// 	vector(iterator start, iterator end){
-	// 		int i;
-	// 		iterator tmp;
+		vector(){
+			v_data = alloc.allocate(1);
+			v_size = 1;
+		}
+		vector(size_t n, T t){
+			v_data = alloc.allocate(n);
+			for(int i = 0; i < n ; i++){
+				v_data[i] = t;
+			}
+		}
+		vector(iterator start, iterator end){
+			int i;
+			iterator tmp;
 
-	// 		tmp = start;
-	// 		for (i = 0; tmp < end; tmp++)
-	// 			i++;
-	// 		alloc.deallocate(v_data, v_size);
-	// 		v_data = alloc.allocate(i +1);
-	// 		v_size = i + 1;
-	// 		for (i = 0; start < end; i++)
-	// 			v_data[i] = *(start++);
+			tmp = start;
+			for (i = 0; tmp < end; tmp++)
+				i++;
+			v_data = alloc.allocate(i +1);
+			v_size = i + 1;
+			for (i = 0; start < end; i++)
+				v_data[i] = *(start++);
 
-	// 	}
-	// 	vector(vector<T> start);
+		}
+		vector(const vector<T, Alloc>& start){
+			iterator it = start.begin();
+
+			v_data = alloc.allocate(start.size());
+			for (it ; it < start.end(); it++)
+				this->v_data = *it;
+		}
 	// // -----------------------------------------------------------------------------------------------------------
 
 
@@ -151,6 +157,40 @@ template < class T, class Alloc = std::allocator<T> > class vector{
 	// // member functions  -----------------------------------------------------------------------------------------------------------
 	// 	template <class InputIterator>  void assign (InputIterator first, InputIterator last);	
 	// 	void assign (size_t n, const T& val);
+		size_t size(){
+			return v_size;
+		}
+		void assign(iterator start, iterator end){
+			int i;
+			iterator tmp;
+
+			tmp = start;
+			for (i = 0; tmp < end; tmp++)
+				i++;
+			v_data = alloc.allocate(i +1);
+			v_size = i + 1;
+			for (i = 0; start < end; i++)
+				v_data[i] = *(start++);
+
+		}
+		void assign(size_t n, T t){
+			v_data = alloc.allocate(n);
+			for(int i = 0; i < n ; i++){
+				v_data[i] = t;
+			}
+		}
+		reference at (size_type n){
+			return &v_data[n];
+		}
+		const_reference at (size_type n) const{
+			return &v_data[n];
+		}
+		reference back(){
+			return &v_data[v_size];
+		}
+		const_reference back() const{
+			return &v_data[v_size];
+		}
 	// // -----------------------------------------------------------------------------------------------------------
 
 };
