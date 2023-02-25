cb0ca4f92e093671229bb74b26fb1dbc9ba6c2e7
Mohamed Ait aadi <mait-aad@e2r3p4.1337.ma>
2023-02-25 18:47:33 +0100

    make the tree red black tree

--- maind.cpp
+++ maind.cpp
@@ -4,21 +4,45 @@ int main()
 {
 {
    Tree<int>* m = new Tree<int>(5);
-
-   m->insert(7);
-   m->insert(2);
-   m->insert(8);
-   Tree<int>* s = new Tree<int>(*m);
-   std::cout << &m->root->right <<">>>>" <<m->root->right->value<< std::endl;
-   std::cout << &m->root->right <<">>>>" <<m->root->lift->value<< std::endl;
-   std::cout << &m->root->right <<">>>>" <<m->root->right->right->value<< std::endl;
-   std::cout << &m->root->right <<">>>>" <<m->root->value<< std::endl;
-   std::cout << &s->root->right <<">>>>" <<s->root->right->value<< std::endl;
-   std::cout << &s->root->right <<">>>>" <<s->root->lift->value<< std::endl;
-   std::cout << &s->root->right <<">>>>" <<s->root->right->right->value<< std::endl;
-   std::cout << &s->root->right <<">>>>" <<s->root->value<< std::endl;
+   Node<int>* a = new Node<int>(7);
+   Node<int>* z = new Node<int>(2);
+   Node<int>* x = new Node<int>(81);
+   Node<int>* c = new Node<int>(8);
+   Node<int>* v = new Node<int>(0);
+   Node<int>* b = new Node<int>(18);
+   Node<int>* f = new Node<int>(5);
+   Node<int>* e = new Node<int>(8);
+   m->insert(a);
+   delete a;
+   m->insert(z);
+   delete z;
+   m->insert(x);
+   delete x;
+   m->insert(c);
+   delete c;
+   m->insert(v);
+   delete v;
+   m->insert(b);
+   delete b;
+   m->insert(f);
+   delete f;
+   m->insert(e);
+   delete e;
+   // Tree<int>* s = new Tree<int>(*m);
+   std::cout << &m->root->right <<"m>>>>" <<m->root->value<< std::endl;
+   std::cout << &m->root->right <<"m>>>>" <<m->root->right->value<< std::endl;
+   std::cout << &m->root->right <<"m>>>>" <<m->root->left->value<< std::endl;
+   std::cout << &m->root->right <<"m>>>>" <<m->root->right->right->value<< std::endl;
+   std::cout << &m->root->right <<"m>>>>" <<m->root->left->value<< std::endl;
+   // std::cout << &s->root->right <<">>>>" <<s->root->right->value<< std::endl;
+   // std::cout << &s->root->right <<">>>>" <<s->root->left->value<< std::endl;
+   // std::cout << &s->root->right <<">>>>" <<s->root->right->right->value<< std::endl;
+   // std::cout << &s->root->right <<"x>>>>" <<s->root->right->value<< std::endl;
+   // s->delete_node(s->root->right);
+   // std::cout << &s->root->right <<"x>>>>" <<s->root->right->value<< std::endl;
+   // std::cout << &s->root->right <<">>>>" <<s->root->value<< std::endl;
    delete m;
-   delete s;
+   // delete s;
 }
    system("leaks a.out");
 }
--- red_black_tree.hpp
+++ red_black_tree.hpp
@@ -4,89 +4,238 @@
 #include <memory>
 
 
+enum Color {red = 1, black = 0};
+template <class T>
+struct Node{
+    Color color;
+    T value;
+    Node *left;
+    Node *right;
+    Node *parent;
+    Node(T val): value(val), parent(nullptr), left(nullptr), right(nullptr){}
+    Node(): value(0), left(nullptr), parent(nullptr), right(nullptr){}
+};
 template <class T>
 class Tree
 {
     private:
-        struct sNode{
-            T value;
-            enum clore {red = 0, black = 0};
-            sNode *lift;
-            sNode *right;
-            sNode(T val): value(val), lift(nullptr), right(nullptr){}
-            sNode(): value(0), lift(nullptr), right(nullptr){}
-        };
-        typedef std::allocator<sNode> Alloc;
+        typedef std::allocator<Node<T> > Alloc;
         Alloc alloc;
     public:
-        sNode *root;
+        typedef Node<T> Node;
+        Node *root;
+        Node *nil;
         Tree(){
+            nil = alloc.allocate(1);
+            nil->left = alloc.allocate(1);
+            nil->right = alloc.allocate(1);
+            *nil = Node();
+            *nil->right = Node();
+            *nil->left = Node();
+            root = nil;
+            this->root->parent = alloc.allocate(1);
+            this->root->parent = nil;
+            this->nil->color = black;
         }
         Tree(T val){
+            nil = alloc.allocate(1);
+            *nil = Node();
+            nil->right = alloc.allocate(1);
+            *nil->right = Node();
+            nil->left = alloc.allocate(1);
+            *nil->left = Node();
             root = alloc.allocate(1);
-            *root = sNode(val);
+            *root = Node(val);
+            root->left = nil;
+            root->right = nil;
+            this->root->parent = nil;
+            this->nil->color = black;
         }
-        sNode  *copy_tree(sNode *tree){
-            sNode *node;
+        Node  *copy_tree(Node *tree, Node *tNil){
+            Node *node;
 
-            if (tree == 0)
-                return 0;
+            if (tree == tNil)
+                return nil;
             node = alloc.allocate(1);
-            alloc.construct(node, sNode(tree->value));
-            node->lift = copy_tree(tree->lift);
-            node->right = copy_tree(tree->right);
+            alloc.construct(node, Node(tree->value));
+            node->parent = tree->parent;
+            node->color = tree->color;
+            node->left = copy_tree(tree->left, tNil);
+            node->right = copy_tree(tree->right, tNil);
             return node;
         }
         Tree(const Tree &tree)
         {
-            this->root = copy_tree(tree.root);
+            nil = alloc.allocate(1);
+            *nil = Node();
+            nil->right = alloc.allocate(1);
+            *nil->right = Node();
+            nil->left = alloc.allocate(1);
+            *nil->left = Node();
+            this->root = copy_tree(tree.root, tree.nil);
+            this->root->parent = nil;
+            this->nil->color = black;
         }
-        void deleteTree(sNode *node)
+        void deleteTree(Node *node)
         {
-            if (node == 0) return;
-            deleteTree(node->lift);
+            if (node == nil)
+                return ;
+            deleteTree(node->left);
             deleteTree(node->right);
-            alloc.deallocate(node, 1);
+            std::cout << node->right << std::endl;
+            if (node != nil)
+                alloc.deallocate(node, 1);
         }
         ~Tree(){
             deleteTree(this->root);
-            // deleteTree(this->right);
-        }
-        void insert(T val)
-        {
-            sNode *n;
+            alloc.deallocate(nil->left, 1);
+            alloc.deallocate(nil->right, 1);
+            alloc.deallocate(nil, 1);
 
-            n = this->root;
-            while (n){
-                if (val < n->value){
-                    if (n->lift == 0){
-                        n->lift = alloc.allocate(1);
-                        alloc.construct(n->lift, sNode());
-                        n = n->lift;
-                        break;
+        }
+        void leftRotate(Node *x){
+            Node *y = x->right;
+            x->right = y->left;
+            std::cout << "::::>"<< x->left->parent<< std::endl <<"::::>"<< x << std::endl;
+            if(y->left != nil)
+                y->left->parent = x;
+            y->parent = x->parent;
+            if (x->parent == nil)
+                this->root = y;
+            else if(x == x->parent->left)
+                x->parent->left = y;
+            else
+                x->parent->right = y;
+            y->left = x;
+            x->parent = y;
+        }
+        void rightRotate(Node *x){
+            Node *y = x->left;
+            x->left = y->right;
+            if(y->right != nil)
+                y->right->parent = x;
+            y->parent = x->parent;
+            if (x->parent == nil)
+                this->root = y;
+            else if(x == x->parent->right)
+                x->parent->right = y;
+            else
+                x->parent->left = y;
+            y->right = x;
+            x->parent = y;
+        }
+        void insertFixUp(Node *z){
+            while (z->parent->color == red){
+                if (z->parent == z->parent->parent->left){
+                    Node *y = z->parent->parent->right;
+                    if (y->color == red){
+                        z->parent->color = black;
+                        y->color = black;
+                        z->parent->parent->color = red;
+                        z = z->parent->parent;
                     }
-                    n = n->lift;
+                    else if(z == z->parent->right){
+                        z = z->parent;
+                        leftRotate(z);
+                    }
+                    else{
+                        z->parent->color = black;
+                        z->parent->parent->color = red;
+                        rightRotate(z->parent->parent);
+                    }
+                    
                 }
                 else{
-                    if (n->right == 0){
-                        n->right = alloc.allocate(1);
-                        alloc.construct(n->right, sNode());
-                        n = n->right;
-                        break;
+                    Node *y = z->parent->parent->left;
+                    if (y->color == red){
+                        z->parent->color = black;
+                        y->color = black;
+                        z->parent->parent->color = red;
+                        z = z->parent->parent;
                     }
-                    n = n->right;
+                    else if(z == z->parent->left){
+                        z = z->parent;
+                        leftRotate(z);
+                    }
+                    else {
+                        z->parent->color = black;
+                        z->parent->parent->color = red;
+                        rightRotate(z->parent->parent);
+                    }
                 }
             }
-            n->value = val;
-            n->lift = 0;
-            n->right = 0;
+            root->color = black;
         }
-        sNode * search(sNode *node, T key){
+        void insert(Node *z)
+        {
+            Node *n = this->root;
+            Node *y = nil;
+
+            while (n != nil){
+                y = n;
+                if (z->value < n->value)
+                    n = n->left;
+                else
+                    n = n->right;
+            }
+            z->parent = y;
+            if (y == nil)
+                this->root = z;
+            else if (z->value < y->value){
+                y->left = alloc.allocate(1);
+                alloc.construct(y->left, *z);
+                y->left->left = nil;
+                y->left->right = nil;
+                y->left->color = red;
+                insertFixUp(y->left);
+            }
+            else{
+                y->right = alloc.allocate(1);
+                alloc.construct(y->right, *z);
+                y->right->left = nil;
+                y->right->right = nil;
+                y->right->color = red;
+                insertFixUp(y->right);
+            }
+            insertFixUp(z);
+        }
+        // void delete_node(Node *z){
+        //     Node *y = z;
+
+        //     if (node->left == nil && node != nil){
+        //         if (node->right != nil)
+        //         {
+        //             tmp = node->right;
+        //             node->value = node->right->value;
+        //             node->left = node->right->left;
+        //             node->right = node->right->right;
+        //             alloc.deallocate(tmp, 1);
+        //         }
+        //         else
+        //             node = nil;
+        //     }
+        //     else if (node->right == nil && node != nil){
+        //         if (node->left != nil){
+        //             tmp = node->right;
+        //             tmp = node->left;
+        //             node->value = node->right->value;
+        //             node->right = node->left->right;
+        //             node->left = node->left->left;
+        //             alloc.deallocate(tmp, 1);
+        //         }
+        //         else
+        //             node = nil;
+        //     }
+        //     else if (node != nil){
+                
+        //     }
+        // }
+        Node * search(Node *node, T key){
             if (node == NULL || node->value == key)
                 return node;
             if (node->value < key)
                 return search(node->right, key);
-            return search(node->lift, key);
+            return search(node->left, key);
         }
 
 };
